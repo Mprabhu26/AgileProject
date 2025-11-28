aeaeec7ac3505cbc3719ea77cfd05c22d16c268d
Ayush Parmar <ayushparmar779@gmail.com>
2025-11-28 21:59:01 +0100

    Add working Spring Boot + Flowable application with BPMN workflow and delegates

---
+++ src/test/java/com/workforce/workforceplanning/WorkforcePlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workforce.workforceplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkforcePlanningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
